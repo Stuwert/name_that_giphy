f2b4f4f68d013c29958165759394a244bf1beb97
Stuart Urback <stuart@urback.net>
2016-03-04 16:58:34 -0700

    Working score

--- front_end/js/controllers.js
+++ front_end/js/controllers.js
@@ -1,17 +1,18 @@
-app.controller('SearchController', ['$scope', 'gifCall', '$location', function($scope, gifCall, $location){
+app.controller('SearchController', ['$scope', 'gifCall', '$location', 'gameService', function($scope, gifCall, $location, gameService){
   $scope.searchForAGif = function(){
     gifCall.searchForAGif($scope.searchTerm).then(function(payload){
       gifCall.setGifs(payload.data);
       $location.path('/game')
     });
   }
+  $scope.score = gameService.score;
 }])
 
 
-app.controller('GameController', ['$scope', 'gifCall', function($scope, gifCall){
+app.controller('GameController', ['$scope', 'gifCall', 'gameService', '$location', function($scope, gifCall, gameService, $location){
   $scope.gifs = gifCall.gifArray;
   console.log($scope.gifs);
-
+  $scope.score = gameService.score;
   $scope.isSelected;
 
   $scope.toggleSelected = function(item){
@@ -20,8 +21,10 @@ app.controller('GameController', ['$scope', 'gifCall', function($scope, gifCall)
 
   $scope.checkAnswer = function(){
     if ($scope.gifs[$scope.isSelected].answer){
+      alert("Correct!")
       gameService.incrementScore();
     }else{
+      alert('You Fail!')
       gameService.resetScore();
     }
     $location.path('/')
