3f5d5b8af79307d7514a76207a9152c113615ff0
Stuwert <stuart@urback.net>
2016-03-06 11:52:56 -0700

    Fiddling with headers

--- express_api/app.js
+++ express_api/app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'jade');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods",  "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
   next();
 });
 // uncomment after placing your favicon in /public
